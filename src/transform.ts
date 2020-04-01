312c411a9fa903cc4a02ac6618d38e316d816dd0
Ivaylo Dankolov <ivodankolov@gmail.com>
2020-04-01 22:08:26 +0300

    Rewrote forEach to only fall back to async iteration if needed.

--- src/transform.ts
+++ src/transform.ts
@@ -1,11 +1,65 @@
-export async function forEach<T>(
-  iterable: Iterable<T> | AsyncIterable<T>,
+import invariant from 'tiny-invariant';
+
+export function forEach<T>(
+  iterable: Iterable<T>,
+  handler: (item: T) => void
+): void;
+export function forEach<T>(
+  iterable: Iterable<T>,
   handler: (item: T) => void | Promise<void>
-) {
-  for await (let item of iterable) {
-    const p = handler(item);
-    if (p instanceof Promise) {
-      await p;
+): Promise<void>;
+export function forEach<T>(
+  iterable: Iterable<T>,
+  handler: (item: T) => void
+): Promise<void>;
+export function forEach<T>(
+  iterable: AsyncIterable<T>,
+  handler: (item: T) => void | Promise<void>
+): Promise<void>;
+export function forEach(iterable: any, handler: any) {
+  if (iterable[Symbol.iterator] != null) {
+    const iterator: Iterator<any> = iterable[Symbol.iterator]();
+    let foundPromise: Promise<any>;
+    while (true) {
+      const next = iterator.next();
+      if (next.done) {
+        return;
+      }
+
+      const handled = handler(next.value);
+      if (handled instanceof Promise) {
+        foundPromise = next.value;
+        break;
+      }
     }
+    // Found a promise, need to fall back to async iteration
+    return async () => {
+      await foundPromise;
+      while (true) {
+        const next = iterator.next();
+        if (next.done) {
+          return;
+        }
+
+        const handled = handler(next.value);
+        if (handled instanceof Promise) {
+          await handled;
+        }
+      }
+    };
+  } else if (iterable[Symbol.asyncIterator] != null) {
+    return (async () => {
+      for await (const item of iterable) {
+        const p = handler(item);
+        if (p instanceof Promise) {
+          await p;
+        }
+      }
+    })();
+  } else {
+    invariant(
+      false,
+      "forEach called with object that's not an Iterable or AsyncIterable"
+    );
   }
 }
