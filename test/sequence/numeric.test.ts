0617157ed5daf80b414b2ba67b7a71d01582f84c
Ivaylo Dankolov <ivodankolov@gmail.com>
2020-04-06 21:28:23 +0300

    Enabled the tests I had commented out for numeric, and added one mroe for infinite seuqences.

--- test/sequence/numeric.test.ts
+++ test/sequence/numeric.test.ts
@@ -1,51 +1,51 @@
 import { seq } from '../../src';
 
 describe('numeric', () => {
-  // it('works for ascending sequences, respecting all 3 parameters', () => {
-  //   const startMin = 0,
-  //     startMax = 10;
-  //   const stepMin = 1,
-  //     stepMax = 10;
-  //   const itemsMin = 1,
-  //     itemsMax = 10;
-  //   for (let start = startMin; start <= startMax; ++start) {
-  //     for (let step = stepMin; step <= stepMax; ++step) {
-  //       for (let items = itemsMin; items < itemsMax; ++items) {
-  //         const stop = start + (items - 1) * step;
-  //         const iterator = seq.numeric({ from: start, to: stop, step });
-  //         for (let i = start; i <= stop; i += step) {
-  //           const next = iterator.next();
-  //           expect(next.value).toEqual(i);
-  //           expect(next.done).toEqual(false);
-  //         }
-  //         expect(iterator.next().done).toEqual(true);
-  //       }
-  //     }
-  //   }
-  // });
+  it('works for ascending sequences, respecting all 3 parameters', () => {
+    const startMin = 0,
+      startMax = 10;
+    const stepMin = 1,
+      stepMax = 10;
+    const itemsMin = 1,
+      itemsMax = 10;
+    for (let start = startMin; start <= startMax; ++start) {
+      for (let step = stepMin; step <= stepMax; ++step) {
+        for (let items = itemsMin; items < itemsMax; ++items) {
+          const stop = start + (items - 1) * step;
+          const iterator = seq.numeric({ from: start, to: stop, step });
+          for (let i = start; i <= stop; i += step) {
+            const next = iterator.next();
+            expect(next.value).toBe(i);
+            expect(next.done).toBe(false);
+          }
+          expect(iterator.next().done).toBe(true);
+        }
+      }
+    }
+  });
 
-  // it('works for descending sequences, respecting all 3 parameters', () => {
-  //   const startMin = 0,
-  //     startMax = 10;
-  //   const stepMin = 1,
-  //     stepMax = 10;
-  //   const itemsMin = 1,
-  //     itemsMax = 10;
-  //   for (let start = startMin; start <= startMax; ++start) {
-  //     for (let step = stepMin; step <= stepMax; ++step) {
-  //       for (let items = itemsMin; items < itemsMax; ++items) {
-  //         const stop = start - (items - 1) * step;
-  //         const iterator = seq.numeric({ from: start, to: stop, step: -step });
-  //         for (let i = start; i >= stop; i -= step) {
-  //           const next = iterator.next();
-  //           expect(next.value).toEqual(i);
-  //           expect(next.done).toEqual(false);
-  //         }
-  //         expect(iterator.next().done).toEqual(true);
-  //       }
-  //     }
-  //   }
-  // });
+  it('works for descending sequences, respecting all 3 parameters', () => {
+    const startMin = 0,
+      startMax = 10;
+    const stepMin = 1,
+      stepMax = 10;
+    const itemsMin = 1,
+      itemsMax = 10;
+    for (let start = startMin; start <= startMax; ++start) {
+      for (let step = stepMin; step <= stepMax; ++step) {
+        for (let items = itemsMin; items < itemsMax; ++items) {
+          const stop = start - (items - 1) * step;
+          const iterator = seq.numeric({ from: start, to: stop, step: -step });
+          for (let i = start; i >= stop; i -= step) {
+            const next = iterator.next();
+            expect(next.value).toBe(i);
+            expect(next.done).toBe(false);
+          }
+          expect(iterator.next().done).toBe(true);
+        }
+      }
+    }
+  });
 
   it('rejects garbage parameter values', () => {
     // const startGarbage = [NaN, +Infinity, -Infinity];
@@ -59,5 +59,24 @@ describe('numeric', () => {
     stepGarbage.forEach(step =>
       expect(() => seq.numeric({ step })).toThrowError()
     );
+  });
+
+  it('works for infinite sequences without taking infinite time', () => {
+    // Testing this to the limit is left as an exercise for the creators of the Simulation.
+    const cutoff = 1000;
+
+    const gen = seq.numeric({ from: 5, step: 10 });
+    for (let i = 0; i < cutoff; ++i) {
+      const { value, done } = gen.next();
+      expect(value).toBe(5 + 10 * i);
+      expect(done).toBe(false);
+    }
+
+    const genNeg = seq.numeric({ from: 42, step: -17 });
+    for (let i = 0; i < cutoff; ++i) {
+      const { value, done } = genNeg.next();
+      expect(value).toBe(42 - 17 * i);
+      expect(done).toBe(false);
+    }
   });
 });
